38361ced33be1ce1efb7e76502ee8551966030e1
Stephen Baidu <stephenbaidu@gmail.com>
2014-04-25 09:50:20 +0000

    Fixed blank value issue

--- lib/resourcify/model/filter_by.rb
+++ lib/resourcify/model/filter_by.rb
@@ -5,6 +5,8 @@ module Model
       column_names = self.column_names
       simple_ops = { eq: '=', lt: '<', gt: '>', lte: '<=', gte: '>=' }
       filters.select { |e| column_names.include?(e.split('.').first) }.each do |key, value|
+        next if value.blank?
+        
         field, operator = key.split('.')
         operator = (operator)? operator.to_sym : :eq
 
