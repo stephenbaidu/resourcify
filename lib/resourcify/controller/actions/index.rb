b13b149e37fcb7d62c859828637e58cdcd157a50
Stephen Baidu <stephenbaidu@gmail.com>
2015-05-28 14:21:53 +0000

    Fixed a bug in index controller which prevents correct filtering in some situations

--- lib/resourcify/controller/actions/index.rb
+++ lib/resourcify/controller/actions/index.rb
@@ -2,14 +2,16 @@ module Controller::Actions
   module Index
     def index
       authorize _RC.new
-
-      @records = policy_scope(_RC.includes(belongs_tos))
+      
+      @records = _RC.includes(belongs_tos)
 
       # apply filter_by if present
       if @records.respond_to? "filter_by"
         @records = @records.filter_by(params.except(:controller, :action, :page, :size))
       end
 
+      @records = policy_scope(@records)
+      
       response.headers['_meta_total'] = @records.count.to_s
 
       page = params[:page] || 1
