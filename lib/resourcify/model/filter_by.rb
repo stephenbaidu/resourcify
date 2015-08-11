2455e5db81af8629bc97a0b195eccd2dc985a29d
Stephen Baidu <stephenbaidu@gmail.com>
2015-08-11 01:45:08 +0000

    Enhanced search with multiple value support

--- lib/resourcify/model/filter_by.rb
+++ lib/resourcify/model/filter_by.rb
@@ -3,30 +3,47 @@ module Model
     def filter_by(filters = {})
       records = self
       column_names = self.column_names
-      simple_ops = { eq: '=', lt: '<', gt: '>', lte: '<=', gte: '>=' }
-      filters.select { |e| column_names.include?(e.split('.').first) }.each do |key, value|
-        next if value.blank?
-        
-        field, operator = key.split('.')
-        operator = (operator)? operator.to_sym : :eq
+      
+      def query_params(q)
+        ops = { eq: '=', lt: '<', gt: '>', lte: '<=', gte: '>=', ne: '!='}
+        field, op, value = q[:name], (q[:op] || 'eq').to_sym, q[:value]
 
-        if simple_ops[operator]
-          records = records.where("#{field} #{simple_ops[operator]} ?", value)
-        elsif operator == :ne
-          records = records.where.not("#{field} = ?", value)
-        elsif operator == :in
-          records = records.where("#{field} IN (?)", value.split(','))
-        elsif operator == :nin
-          records = records.where.not("#{field} IN (?)", value.split(','))
-        elsif operator == :like
-          if ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
-            records = records.where("#{field} ILIKE ?", "%#{value}%")
-          else
-            records = records.where("#{field} LIKE ?", "%#{value}%")
-          end
+        if ops[op]
+          ["#{field} #{ops[op]} ?", value]
+        elsif op == :in
+          ["#{field} IN (?)", value.split(',')]
+        elsif op == :nin
+          ["#{field} NOT IN (?)", value.split(',')]
+        elsif op == :between
+          vals = value.split('||')
+          query_string = '(' + vals.map { |e| "(#{field} >= ? AND #{field} <= ?)" }.join(' OR ') + ')'
+          vals = vals.map { |e| e.split('|') }.flatten
+          vals.unshift(query_string)
+          vals
+        elsif op == :like
+          like_key = (ActiveRecord::Base.connection.adapter_name == "PostgreSQL")? 'ILIKE' : 'LIKE'
+          vals = value.split('|').map { |val| "%#{val}%" }
+          query_string = '(' + vals.map { |e| "#{field} #{like_key} ?" }.join(' OR ') + ')'
+          vals.unshift(query_string)
+          vals
         else
-          records = records.where("#{field} = ?", value)
+          ["#{field} = ?", value]
         end
+      end
+
+      filters.map do |key, value|
+        name, op = key.to_s.split('.')
+        { name: name, op: op, value: value }
+      end.group_by do |q|
+        q[:name]
+      end.select do |key|
+        column_names.include? key
+      end.map do |key, value|
+        qparams = value.map { |e| query_params(e) }
+        qstring = qparams.map { |e| e.first }.join(' OR ')
+        qparams.map { |e| e[1..-1] }.flatten(1).unshift(qstring)
+      end.each do |qparam|
+        records = records.where(*qparam)
       end
       
       records
